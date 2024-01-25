5306220d54fce8fd676cf3373b7ad6ad22b75c28
Aatish <aatishda1@gmail.com>
2024-01-25 20:36:53 +0000

    Added Dataset to Memberzone

--- website/views.py
+++ website/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+# Database Connection
+import mysql.connector    
+cnx = mysql.connector.connect(user='root', password='password',
+                              host='localhost',
+                              database='licensedatav2')
+
 def index(request):
     return render(request, 'index.html', {})
 
@@ -7,4 +13,14 @@ def about(request):
     return render(request, 'about.html', {})
 
 def member_zone(request):
-    return render(request, 'member_zone.html', {})
+    # Fetching data
+    result = []
+    try:
+        cursor = cnx.cursor()
+        cursor.execute("""
+            SELECT id, `First Name`, `Last Name`, `Date of Birth`, `Place of Birth`, `Gender`, `Date of Issue`, `Date of Expiry`, `Issuing Authority`, `License Number`, `Address`, `Dataset` FROM licensedatav2.license_data;
+        """)
+        result = cursor.fetchall()
+    except:
+        pass
+    return render(request, 'member_zone.html', {"data":result})
